b52146927ad48cfabb96a6c8ef54fd9d4a693971
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2023-08-23 23:31:46 +0200

    fetching data exemple and use it in components

--- fullstackapp_next_js/src/app/blog/[id]/page.jsx
+++ fullstackapp_next_js/src/app/blog/[id]/page.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import styles from '../style.module.css'
+import { notFound } from 'next/navigation'
 
-const postblog = () => {
+async function getData(id){
+  const res=await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+  if(!res.ok){
+    return notFound()
+  }
+  return res.json();  
+}
+
+const postblog = async({params}) => {
+  const data=await getData(params.id)
+  console.log(data)
   return (
-    <div>postblog</div>
+    <div>
+                  <div className={styles.content}>
+                  <h1 className={styles.title}>{data.title}</h1>
+                  <p className={styles.desc}>{data.desc}</p>
+                  </div>
+
+
+
+    </div>
+
   )
 }
 
--- fullstackapp_next_js/src/app/blog/page.jsx
+++ fullstackapp_next_js/src/app/blog/page.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
-import style from './style.module.css'
+import styles from './style.module.css'
+import Link from 'next/link';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+async function getData(){
+  const res=await fetch('https://jsonplaceholder.typicode.com/posts',{
 
-const blog = () => {
+    cache: "no-store", // data changable each request no make data cach in browser..
+    // { cache: 'force-cache' }  // make data cach in web browser imediatly cause is not changable it's static
+    // fetch('https://...', { next: { revalidate: 3600 } }) Revalidating Data make cach store each second semi dynamic
+
+
+
+  })
+  if(!res.ok){
+      return notFound()
+  }
+  return res.json();
+}
+
+const blog = async() => {
+  const data=await getData();
   return (
-    <div className={style.container}>page</div>
-  )
+    <div className={styles.mainContainer}>
+      {data.map((item) => (
+        <Link href={`/blog/${item.id}`} className={styles.container} key={item.id}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={item.img}
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+          </div>
+        </Link>
+      ))}
+    </div> 
+     )
 }
 
 export default blog
